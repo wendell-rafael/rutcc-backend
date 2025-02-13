9dcb01e4921fc581f5cefb500b87e2fecf0718fb
Wendell Rafael <wendell.nascimento@splab.ufcg.edu.br>
2025-02-13 14:51:41 -0300

    ajuste no db

--- app/models/cardapio.py
+++ app/models/cardapio.py
@@ -1,16 +1,19 @@
-# app/models/cardapio.py
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime, UniqueConstraint
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql import func
+from sqlalchemy.dialects.postgresql import ENUM
 
 Base = declarative_base()
 
+refeicao_tipo = ENUM('Almoço', 'Jantar', name='refeicao_tipo', create_type=True)
 
 class Cardapio(Base):
     __tablename__ = "cardapios"
+    __table_args__ = (UniqueConstraint('dia', 'refeicao', name='unique_dia_refeicao'),)
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    refeicao = Column(String, nullable=False)  # Ex.: "Almoço" ou "Jantar"
-    dia = Column(Integer, nullable=False)  # Dia do mês
+    refeicao = Column(refeicao_tipo, nullable=False, index=True)
+    dia = Column(Integer, nullable=False, index=True)
     opcao1 = Column(String, nullable=True)
     opcao2 = Column(String, nullable=True)
     opcao_vegana = Column(String, nullable=True)
@@ -25,3 +28,5 @@ class Cardapio(Base):
     sobremesa = Column(String, nullable=True)
     cafe = Column(String, nullable=True)
     pao = Column(String, nullable=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
